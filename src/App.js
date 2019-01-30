bd85d54e014c9115938e2615d5947638fe4ab841
andytan2624 <andy.tan2624@gmail.com>
2019-01-31 08:18:19 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. Bo gech ger walch ss
           </p>
           <a
             className="App-link"
@@ -17,9 +17,11 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+
           </a>
         </header>
+
+        <p className="app-intro">Hi There</p>
       </div>
     );
   }
